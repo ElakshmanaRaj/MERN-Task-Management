3d9b8ba7d83d94b01ea71a688375fe3c1e610d80
ElakshmanaRaj <elakshmanabca07@gmail.com>
2025-08-10 12:23:33 +0530

    upload image fix

--- Frontend/task-management/src/utils/uploadImage.js
+++ Frontend/task-management/src/utils/uploadImage.js
@@ -1,28 +1,23 @@
 import { API_PATHS } from "./apiPath";
 import axiosInstance from "./axiosInstance";
 
-const uploadImage = async (imageFile)=>{
-    const formData = new FormData();
+const uploadImage = async (imageFile) => {
+  const formData = new FormData();
+  formData.append("image", imageFile); // must match upload.single("image") in backend
 
-    // Upload if any image
-    formData.append("image", imageFile);
-
-    try {
-
-        const response = await axiosInstance.post(API_PATHS.IMAGE.UPLOAD_IMAGE, formData,{
-            headers:{
-                "Content-Type":"multipart/form-data",
-            },
-        });
-
-        return response.data;
-
-    } catch (error) {
-        console.error ("Error uploading the image", error)
-        throw  error;
-        
-    }
-
-} 
+  try {
+    const response = await axiosInstance.post(
+      API_PATHS.IMAGE.UPLOAD_IMAGE, // <-- use correct full API path
+      formData,
+      {
+        headers: { "Content-Type": "multipart/form-data" },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error uploading the image", error);
+    throw error;
+  }
+};
 
 export default uploadImage;
